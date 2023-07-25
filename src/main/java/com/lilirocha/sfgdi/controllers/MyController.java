3f731df66250caa4fab60875650bf031ef5fe266
Lillian Rocha <lillianrocha.negocio@gmail.com>
2023-07-25 18:03:00 -0300

    Application Context example

---
+++ src/main/java/com/lilirocha/sfgdi/controllers/MyController.java
@@ -0,0 +1,12 @@
+package com.lilirocha.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello() {
+        System.out.println("Hello world!!!");
+        return "Hi Folks!";
+    }
+
+}
