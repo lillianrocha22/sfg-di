4f5c61045f87673984600544b892f74e70844bac
Lillian Rocha <lillianrocha.negocio@gmail.com>
2023-07-25 20:07:33 -0300

    Spring Profiles example

--- src/main/java/com/lilirocha/sfgdi/SfgDiApplication.java
+++ src/main/java/com/lilirocha/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package com.lilirocha.sfgdi;
 
-import com.lilirocha.sfgdi.controllers.ConstructorInjectedController;
-import com.lilirocha.sfgdi.controllers.MyController;
-import com.lilirocha.sfgdi.controllers.PropertyInjectedController;
-import com.lilirocha.sfgdi.controllers.SetterInjectedController;
+import com.lilirocha.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,10 @@ public class SfgDiApplication {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+        System.out.println(i18nController.sayHello());
+
         MyController myController = (MyController) ctx.getBean("myController");
 
         System.out.println("---Primary Bean");
