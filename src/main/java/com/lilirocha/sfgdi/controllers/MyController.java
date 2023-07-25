6e684ace8a0c60bb4544ac9e2998d52534184cc6
Lillian Rocha <lillianrocha.negocio@gmail.com>
2023-07-25 19:48:01 -0300

    Primary Beans example

--- src/main/java/com/lilirocha/sfgdi/controllers/MyController.java
+++ src/main/java/com/lilirocha/sfgdi/controllers/MyController.java
@@ -1,12 +1,19 @@
 package com.lilirocha.sfgdi.controllers;
 
+import com.lilirocha.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello() {
-        System.out.println("Hello world!!!");
-        return "Hi Folks!";
+        return greetingService.sayGreeting();
     }
 
 }
